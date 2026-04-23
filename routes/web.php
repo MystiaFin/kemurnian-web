34cc0771acdc530247e624cea9f9ef62764e677e
MystiaFin <cytrsx01@gmail.com>
2026-04-23 16:05:43 +0700

    feat: add admin kurikulum routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\HeroController;
+use App\Http\Controllers\Admin\KurikulumController;
 
 Route::prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index']);
@@ -10,11 +11,21 @@ Route::prefix('admin')->group(function () {
 
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Hero
     Route::get('/hero', [HeroController::class, 'index'])->name('hero');
     Route::get('/hero/create', [HeroController::class, 'create'])->name('hero.create');
     Route::post('/hero', [HeroController::class, 'store'])->name('hero.store');
     Route::delete('/hero/{id}', [HeroController::class, 'destroy'])->name('hero.destroy');
     Route::post('/hero/reorder', [HeroController::class, 'reorder'])->name('hero.reorder');
+
+    // Kurikulum
+    Route::get('/kurikulum', [KurikulumController::class, 'index'])->name('kurikulum');
+    Route::get('/kurikulum/create', [KurikulumController::class, 'create'])->name('kurikulum.create');
+    Route::post('/kurikulum', [KurikulumController::class, 'store'])->name('kurikulum.store');
+    Route::get('/kurikulum/edit/{id}', [KurikulumController::class, 'edit'])->name('kurikulum.edit');
+    Route::put('/kurikulum/{id}', [KurikulumController::class, 'update'])->name('kurikulum.update');
+    Route::delete('/kurikulum/{id}', [KurikulumController::class, 'destroy'])->name('kurikulum.destroy');
 });
 
 Route::get('/debug-hero', function () {
