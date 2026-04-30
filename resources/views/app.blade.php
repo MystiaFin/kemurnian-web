1521e81a65cd8a41adcd082df3dbb5a666c4f835
MystiaFin <cytrsx01@gmail.com>
2026-04-30 09:02:24 +0700

    feat: title and meta description in blade root

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <title inertia>Sekolah Kemurnian</title>
+    <meta name="description" content="Sekolah Kemurnian, berdiri sejak 1978 di Jakarta Barat..." inertia>
     @viteReactRefresh
     @vite(['resources/js/app.tsx', 'resources/css/app.css'])
     @inertiaHead
